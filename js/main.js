fe6c78775d1af61610b69c3bd6f32d573b3f4c87
lauren <laurencrosariol@yahoo.com>
2022-04-08 16:28:12 -0700

    some pseudo code added, not yet complete

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/** CONSTANTS **/
+    /** Will need:
+     *      -a colors object with keys for 2 players and 
+     *       a 3rd  key with a value of 'null; for the unused 
+     *       box on the grid
+     *      -an array to track the winning combinations OR 
+     *       use a function instead which may be useful in 
+     *       clearing the board for reset; also typing a bunch 
+     *       of numbers in an array may be easier than typing 
+     *       a bunch of functions to determine the win
+    **/
+/** VARIABLES **/
+    /** Will probably need: 
+     *      -board
+     *      -player turns
+     *      -winner
+     **/
+/** CACHED ELEMENT REFERENCES **/
+    /** Will need: 
+     *      -buttons for top of game
+     *      -replay button
+     *      -winner display
+     *      -load squares on html to js (td or div)
+    **/
+/** EVENT LISTENERS **/
+    /** Will need: 
+     *       -button click
+     *       -replay click
+    **/
+/** FUNCTIONS **/
+    /** Will need: 
+     *        -a board initialization that makes all the 
+     *         boxes 'null', turn, winner variables
+     *        -a render function to loop over the squares, 
+     *         use index to change the color of the square
+     *         upon player selection; also winner message
+     *        -a function to track the choices made by 
+     *         players/and check for a win like TTT but 
+     *         4 positions instead of 3 and ===4 instead of 3;
+     *         also look for a tie if ===42(spots on board and no win)
+     *        -if board includes null return null 
+     *        -**/
