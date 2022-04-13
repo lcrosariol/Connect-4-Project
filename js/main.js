76109e1723bd378d4b8972fd41cb8a8b08abb0f1
Lauren Crosariol <laurencrosariol@yahoo.com>
2022-04-13 15:23:17 -0700

    corrected

--- js/main.js
+++ js/main.js
@@ -1,137 +1,47 @@
-
-
-// IF THAT ONE DOESN"T WORK THIS DOES!!!!
-
-
 /** CONSTANTS **/
-    /** Will need:
-     *      -a colors object with keys for 2 players and 
-     *       a 3rd  key with a value of 'null; for the unused 
-     *       box on the grid
-     *      -an array to track the winning combinations OR 
-     *       use a function instead which may be useful in 
-     *       clearing the board for reset; also typing a bunch 
-     *       of numbers in an array may be easier than typing 
-     *       a bunch of functions to determine the win
-    **/
-const colors = {
-    '1': "red",
-    '-1': "yellow",
+
+const playerColors = {
+    '1': "#cc2900",
+    '-1': "#e6b800",
     '0': "#838282"
 }
 
 /** VARIABLES **/
-    /** Will probably need: 
-     *      -board
-     *      -player turns
-     *      -winner
-     **/
+
 let board;
 let currentPlayer;
 let winner;
 let totalTurns;
 
-
-
-// // Constants
-// const sounds = {
-//     laser: 'http://www.freesound.org/data/previews/42/42106_70164-lq.mp3',
-//     dog: 'http://www.freesound.org/data/previews/327/327666_5632380-lq.mp3',
-//     cow: 'http://www.freesound.org/data/previews/58/58277_634166-lq.mp3',
-//     siren: 'http://www.freesound.org/data/previews/336/336899_4939433-lq.mp3'
-// };
-
-// // Cached DOM Elements
-// const player = new Audio();
-// const bgPlayer = document.getElementById('bg-player');
-// const bgCheckbox = document.querySelector('input[type="checkbox"]');
-
-// // Adjust volume of background music to 50%
-// bgPlayer.volume = .3;
-
-// // Event Listeners
-// document.getElementById('play-btn').addEventListener('click', handlePlaySound);
-// bgCheckbox.addEventListener('change', handleBgChanged);
-
-// // Functions
-// function handlePlaySound() {
-//   // Use the CSS :checked pseudoclass to select the selected radio button
-//     const selSoundInp = document.querySelector('input[name="sound"]:checked');
-//     playSound(selSoundInp.value);
-// }
-
-// function playSound(name) {
-//     player.src = sounds[name];
-//     player.play();
-// }
-
-// function handleBgChanged() {
-//     bgCheckbox.checked ? bgPlayer.play() : bgPlayer.pause();
-// }
-
-
-
-
-
-
-
-
 /** CACHED ELEMENT REFERENCES **/
-    /** Will need: 
-     *      -buttons for top of game
-     *      -replay button
-     *      -winner display
-     *      -load squares on html to js (td or div)
-    **/
-const colBtns = [...document.querySelectorAll("#topButtons > button")]; // selects the first button with 
-//#topButtons as the parent element
-console.log(colBtns);
-//query selector all is array like but can't use array methods on buttons
-// const squares = document.querySelectorAll("#gameboard")
+
+const colBtns = [...document.querySelectorAll("#topButtons > button")];
 const resetButton = document.getElementById("resetGame")
-const headerEl = document.querySelector("#displayMessage")
+const headerEl = document.getElementById("displayMessage")
 const player = new Audio();
+const drum = document.getElementById('drum');
+drum.volume = 0.3;
+const winnerSound = document.getElementById("winner");
+winnerSound.volume = 0.4;
 
 /** EVENT LISTENERS **/
-    /** Will need: 
-     *       -button click
-     *       -replay click
-    **/
 
 document.getElementById("topButtons").addEventListener("click", playerTakesTurn);
 document.getElementById("resetGame").addEventListener("click", init);
-// document.querySelector(".top").addEventListener("click", playerTakesTurn);
-
 
 /** FUNCTIONS **/
-    /** Will need: 
-     *        -a board initialization that makes all the 
-     *         boxes 'null', turn, winner variables
-     *        -a render function to loop over the squares, 
-     *         use index to change the color of the square
-     *         upon player selection; also winner message
-     *        -a function to track the choices made by 
-     *         players/and check for a win like TTT but 
-     *         4 positions instead of 3 and ===4 instead of 3;
-     *         also look for a tie if ===42(spots on board and no win)
-     *        -or search for square by background color to compare against 
-     *         winning combinations.
-     *        -if board includes null return null 
-     *        
-     *        -**/
-
 
 init();
 
 function init() {
     board = [
-        [0, 0, 0, 0, 0, 0],  // Column 0
-        [0, 0, 0, 0, 0, 0],  // Column 1
-        [0, 0, 0, 0, 0, 0],  // Column 2
-        [0, 0, 0, 0, 0, 0],  // Column 3
-        [0, 0, 0, 0, 0, 0],  // Column 4
-        [0, 0, 0, 0, 0, 0],  // Column 5
-        [0, 0, 0, 0, 0, 0],  // Column 6
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
     ];
     currentPlayer = 1;
     totalTurns = 0;
@@ -140,50 +50,46 @@ function init() {
 }
 
 function render() {
-//     /** Iterates over each column in the board. **/
-    board.forEach((column, columnidx) => {
-//         /** Iterates over each cell in the column. **/
-        column.forEach((cell, cellidx) => {
-//             /** Targets the specific cell by using the index of both column and cell. **/
-            let div = document.getElementById(`c${columnidx}r${cellidx}`);
-//             /** Applies a background style to the cell from colors listed in constants. **/
-            div.style.backgroundColor = colors[cell];
+    board.forEach((column, columnIndex) => {
+        column.forEach((cell, cellIndex) => {
+            let div = document.getElementById(`c${columnIndex}r${cellIndex}`);
+            div.style.backgroundColor = playerColors[cell];
         });
-//     /** If column is full make the button above the column invisible (If it includes a 0 (white square), it is visible, if not it is hidden.)**/
-        colBtns[columnidx].style.visibility = column.includes(0) ? "visible" : "hidden";
+        colBtns[columnIndex].style.visibility = column.includes(0) ? "visible" : "hidden";
     });
-//     /** Adds to total turns because when we reach 42, the board is full. **/
     resetGame();
     totalTurns++;
 }
 
 function playerTakesTurn(evt) {
-    const  colIdx = colBtns.indexOf(evt.target);
-    if (colIdx === -1 || winner) return;
+    const colIdx = colBtns.indexOf(evt.target);
+    if (colIdx === -1 || winner) return false;
     const colArr = board[colIdx];
-    const rowIdx = colArr.indexOf(0);
-    if (rowIdx === -1) return;
+    const rowIdx = colArr.indexOf(0);                                                               ////????? I am not sure I understand this correctly
+    if (rowIdx === -1) return false;
     colArr[rowIdx] = currentPlayer;
     currentPlayer *= -1;
     winner = checkForWin(colIdx, rowIdx);
+    drum.play();
     render();
-    
+
     if (winner === 'T') {
-        displayMessage.innerText = "It's a Tie!";
+        displayMessage.textContent = "It's a Tie!";
     } else if (winner) {
-        displayMessage.innerText = `${winner=== 1 ? 'RED' : 'YELLOW'} is the Winner!`;
+        winnerSound.play();
+        displayMessage.textContent = `${winner === 1 ? 'RED' : 'YELLOW'} is the Winner!`;
     } else {
-        headerEl.innerHTML = `${currentPlayer === 1 ? 'RED' : 'YELLOW'}'s turn.`;
+        headerEl.textContent = `${currentPlayer === 1 ? 'RED' : 'YELLOW'}'s turn.`;
     }
 }
 
 function checkForWin() {
-    if (totalTurns >= 42) {return winner = "T"};
+    if (totalTurns >= 42) { return winner = "T" };
     for (let colIdx = 0; colIdx <= 6; colIdx++) {
         winner = checkCol(colIdx);
         if (winner) break;
     }
-    return winner; 
+    return winner;
 }
 
 function checkCol(colIdx) {
@@ -199,7 +105,7 @@ function checkCol(colIdx) {
 function checkDiag(colIdx, rowIdx, dir) {
     if (dir > 0 && colIdx > 3 || dir > 0 && rowIdx > 2) return null;
     if (dir < 0 && colIdx > 3 || dir < 0 && rowIdx < 3) return null;
-    if (Math.abs(board[colIdx][rowIdx] + board[colIdx + 1][rowIdx + dir] + board[colIdx + 2][rowIdx + dir * 2]+ board[colIdx + 3][rowIdx + dir * 3]) === 4) {
+    if (Math.abs(board[colIdx][rowIdx] + board[colIdx + 1][rowIdx + dir] + board[colIdx + 2][rowIdx + dir * 2] + board[colIdx + 3][rowIdx + dir * 3]) === 4) {
         return board[colIdx][rowIdx];
     } else {
         return null;
@@ -227,8 +133,52 @@ function checkVert(colArr, rowIdx) {
 
 function resetGame() {
     if (winner) {
-        resetButton.innerText = "Reset Game?";
+        resetButton.textContent = "Reset Game?";
     } else {
-        resetButton.innerText = "Give up?";
+        resetButton.textContent = "Give up?";
     }
 }
+
+/** Code snippet from Nash Vail https://codepen.io/nashvail/pen/wpGgXO **/
+// Some random colors
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 50;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+    let ball = document.createElement("div");
+    ball.classList.add("ball");
+    ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+    ball.style.transform = `scale(${Math.random()})`;
+    ball.style.width = `${Math.random()}em`;
+    ball.style.height = ball.style.width;
+
+    balls.push(ball);
+    document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+    let to = {
+        x: Math.random() * (i % 2 === 0 ? -11 : 11),
+        y: Math.random() * 12
+    };
+
+    let anim = el.animate(
+        [
+            { transform: "translate(0, 0)" },
+            { transform: `translate(${to.x}rem, ${to.y}rem)` }
+        ],
+        {
+            duration: (Math.random() + 1) * 2000, // random duration
+            direction: "alternate",
+            fill: "both",
+            iterations: Infinity,
+            easing: "ease-in-out"
+        }
+    );
+});
+/** Code snippet from Nash Vail https://codepen.io/nashvail/pen/wpGgXO **/
---
+++ js/notes.js
@@ -0,0 +1,47 @@
+// Calls initialization function.  Can be called before the function because of hoisting.
+
+// function init() {                                    Runs Initialization (init) function.
+//     board = [                                        Creates a constant for the array that is going to be our game board.
+//         [0, 0, 0, 0, 0, 0],  // Column 0
+//         [0, 0, 0, 0, 0, 0],  // Column 1
+//         [0, 0, 0, 0, 0, 0],  // Column 2
+//         [0, 0, 0, 0, 0, 0],  // Column 3
+//         [0, 0, 0, 0, 0, 0],  // Column 4
+//         [0, 0, 0, 0, 0, 0],  // Column 5
+//         [0, 0, 0, 0, 0, 0],  // Column 6
+//     ];
+// First player (red) is set to 1.
+//  The total turns is set to 0.  Remember there are only 42 squares on the board, so at 42 we have a tie.
+
+// The winner is set to null, which means nothing.
+
+
+// Renders the HTML to the browser.
+// Iterates over each column in the constant board one time.  The parameters are column (element) and columnIndex (index).
+// Iterates over each cell in the constant board one time.  The parameters are cell (element) and cellIndex (index).
+// Targets the specific cell by using the index of both column and cell. Renders the cells to the HTML divs.  Now we have a playable game board.
+// Applies a background style to the cell from colors listed in constants. We start off with grey as all the cells have a value of 0 until played.
+
+// If column is full make the button above the column invisible (If it includes a 0 (grey square), it is visible, if not it is hidden.)
+
+// Runs a function to see if the game reset button needs to change its text content.
+// Adds to total turns because when we reach 42, the board is full.
+
+
+// When a player takes a turn, the event checks the index of the cell on the column...
+// Declaring a constant colIdx, which is the index of the button that is clicked on (evt). .indexOf returns the first index at which a given element can be found in the array, or -1 if it is not present.
+//          So in other words if you can't click on it it will return -1. // If the board is full or we have a winner, return false.
+// Declaring a constant colArr, which is the index of the column of the button clicked on.
+// Declaring a constant rowIdx, which is the index of colArr(the index of the column of the button clicked on) at index 0(row starting at 0(bottom)).
+// IndexOf returns -1 if outside the array.  So if it is -1 return false.
+// The index of the column of the button clicked on at the row index (So the first spot under the button clicked) gets set to the number value of the player (1 or -1).
+// The number value of the player gets multiplied by -1, so the turn changes.
+// We check for a winner by running the checkForWin function, and passing to it the colIdx and rowIdx. IE the player number at that coordinate.
+// Play drum sound.
+// Call render function.
+
+// If winner is set to 'T', display "It's a Tie!".
+
+// Else if we have a winner, play win sound and
+// display which player is the winner.
+// If there's not a tie or winner, display who's turn it is.   .textContent or .innerText work fine here.
